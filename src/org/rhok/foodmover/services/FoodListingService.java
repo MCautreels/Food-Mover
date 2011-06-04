e7b848b6bde6723522a9acef310757108b5f5fbb
tim <tim.vaes@miiatech.com>
2011-06-04 14:04:53 -0700

    Added persistency files

---
+++ src/org/rhok/foodmover/services/FoodListingService.java
@@ -0,0 +1,71 @@
+package org.rhok.foodmover.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import org.rhok.foodmover.database.EMF;
+import org.rhok.foodmover.entities.FoodListing;
+
+public class FoodListingService {
+	
+	public void addFoodListing(FoodListing foodlisting){
+		EntityManagerFactory emf = EMF.get();
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction tx =  em.getTransaction();
+		try
+		{
+		    tx.begin();		    
+		    em.persist(foodlisting);
+		    tx.commit();
+		}
+		finally
+		{
+		    if (tx.isActive())
+		    {
+		        tx.rollback();
+		    }
+		    em.close();
+		}
+	}
+	
+	public List<FoodListing> getFoodListingByName(String name)
+	{
+		List<FoodListing> resultList = new ArrayList<FoodListing>();
+		EntityManagerFactory emf = EMF.get();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		try
+		{
+		    tx.begin();
+
+		    Query q = em.createQuery("SELECT fl FROM FoodListing fl WHERE fl.name = '" + name + "'");
+		    List results = q.getResultList();
+		    Iterator iter = results.iterator();
+		    
+		    while (iter.hasNext())
+		    {
+		        FoodListing fl = (FoodListing) iter.next();
+		        resultList.add(fl);
+		    }
+		    		    
+		    tx.commit();
+		}
+		finally
+		{
+		    if (tx.isActive())
+		    {
+		        tx.rollback();
+		    }
+
+		    em.close();
+		}
+		return resultList;
+	}
+}
