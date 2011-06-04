5368535e0ec437c482a852a062f1b4b98fec489c
unknown <Nick@.(none)>
2011-06-04 13:26:46 -0700

    Changing .com to .org and adding unit tests.

---
+++ src/org/rhok/foodmover/test/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.rhok.foodmover.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SimpleTest {
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
