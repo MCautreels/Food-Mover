d981ecc38aa3d933def0aca3bf0138f27c728822
NickHeiner <nth23@cornell.edu>
2011-06-04 22:27:42 -0700

    Implementing the "update" API.
    Trying to make it work on the front end but it doesn't.

--- src/org/rhok/foodmover/api/ListingServlet.java
+++ src/org/rhok/foodmover/api/ListingServlet.java
@@ -15,6 +15,7 @@ import org.rhok.foodmover.entities.FoodMoverUser;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
@@ -26,90 +27,121 @@ import com.google.appengine.repackaged.org.json.JSONStringer;
 public class ListingServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		if(req.getParameter("lat") == null)	throw new IllegalStateException("No lat parameter set");
-		if(req.getParameter("lng") == null)	throw new IllegalStateException("No lng parameter set");
-		if(req.getParameter("description") == null)	throw new IllegalStateException("No description parameter set");
-		if(req.getParameter("quantity") == null) throw new IllegalStateException("No quantity parameter set");
-		
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		checkParams(req);
+
 		float lat = Float.parseFloat(req.getParameter("lat"));
 		float longitude = Float.parseFloat(req.getParameter("lng"));
 		String description = req.getParameter("description");
 		int quantity = Integer.parseInt(req.getParameter("quantity"));
-		
+
 		FoodListing listing = new FoodListing();
 		listing.setLat(lat);
 		listing.setLongitude(longitude);
 		listing.setDescription(description);
 		listing.setQuantity(quantity);
 		listing.setOwner(FoodMoverUser.getCurrentUser());
-		
+
 		listing.put();
-		
+
+		writeKey(resp, listing);
+	}
+
+	private void writeKey(HttpServletResponse resp, FoodListing listing) throws IOException {
 		resp.getWriter().println(KeyFactory.keyToString(listing.getKey()));
 	}
-	
+
+	private void checkParams(HttpServletRequest req) {
+		if (req.getParameter("lat") == null)
+			throw new IllegalStateException("No lat parameter set");
+		if (req.getParameter("lng") == null)
+			throw new IllegalStateException("No lng parameter set");
+		if (req.getParameter("description") == null)
+			throw new IllegalStateException("No description parameter set");
+		if (req.getParameter("quantity") == null)
+			throw new IllegalStateException("No quantity parameter set");
+	}
+
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException {
-		if(req.getParameter("key") == null)	throw new IllegalStateException("No key parameter set");
-		
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		checkParams(req);
+		final String strKey = req.getParameter("key");
+		if (strKey == null)
+			throw new IllegalStateException("No key parameter set");
+
+		Key key = KeyFactory.stringToKey(strKey);
+		try {
+			FoodListing listing = new FoodListing(DatastoreServiceFactory.getDatastoreService().get(key));
+			listing.setDescription(req.getParameter("description"));
+			listing.setLat(Float.parseFloat(req.getParameter("lat")));
+			listing.setLongitude(Float.parseFloat(req.getParameter("long")));
+			listing.setQuantity(Integer.parseInt(req.getParameter("quantity")));
+			listing.setOwner(FoodMoverUser.getCurrentUser());
+			
+			listing.put();
+			
+			writeKey(resp, listing);
+			
+		} catch (EntityNotFoundException e) {
+			throw new IllegalStateException("key does not match any current food listing. Did you use KeyFactory.keyToString() correctly?");
+		}
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (req.getParameter("key") == null)
+			throw new IllegalStateException("No key parameter set");
+
 		Key key = KeyFactory.stringToKey(req.getParameter("key"));
 		FoodListing fl = new FoodListing(key);
-		
-		if(fl.getOwner().getRawUserObject().equals(FoodMoverUser.getCurrentUser().getRawUserObject()))
-		{
+
+		if (fl.getOwner().getRawUserObject().equals(FoodMoverUser.getCurrentUser().getRawUserObject())) {
 			fl.delete();
-		}
-		else {
+		} else {
 			throw new IllegalAccessError("You have to be the owner of the listing");
 		}
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/plain");
-		
+
 		List<FoodListing> foodlistings = null;
-		
-		if(req.getParameter("lat") != null && req.getParameter("lng") != null) {
+
+		if (req.getParameter("lat") != null && req.getParameter("lng") != null) {
 			float latitude = Float.parseFloat(req.getParameter("lat"));
-			float longitude = Float.parseFloat(req.getParameter("lng"));		
+			float longitude = Float.parseFloat(req.getParameter("lng"));
 			float distance;
-			if(req.getParameter("distance") == null){
+			if (req.getParameter("distance") == null) {
 				distance = Float.parseFloat(this.getInitParameter("default_distance"));
 			} else {
 				distance = Float.parseFloat(req.getParameter("distance"));
-			}	
+			}
 			foodlistings = findFoodListings(longitude, latitude, distance);
-		}
-		else if(FoodMoverUser.getCurrentUser() != null){
+		} else if (FoodMoverUser.getCurrentUser() != null) {
 			foodlistings = getMyFoodListings();
-		}
-		else {
+		} else {
 			JSONStringer jsonStringer = new JSONStringer();
 			try {
-				resp.getWriter().println(jsonStringer.object().key("ERROR").value("Not logged in / No lng or lat set").toString());
+				resp.getWriter().println(
+						jsonStringer.object().key("ERROR").value("Not logged in / No lng or lat set").toString());
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		JSONStringer jsonStringer = new JSONStringer();
 		try {
 			jsonStringer.array();
 			for (FoodListing foodListing : foodlistings) {
-				jsonStringer.object()
-				.key("id").value(KeyFactory.keyToString(foodListing.getKey()))
-				.key("lat").value(foodListing.getLat())
-				.key("lng").value(foodListing.getLongitude())
-				.key("description").value(foodListing.getDescription())
-				.key("quantity").value(foodListing.getQuantity()).endObject();
+				jsonStringer.object().key("id").value(KeyFactory.keyToString(foodListing.getKey())).key("lat")
+						.value(foodListing.getLat()).key("lng").value(foodListing.getLongitude()).key("description")
+						.value(foodListing.getDescription()).key("quantity").value(foodListing.getQuantity())
+						.endObject();
 			}
 			jsonStringer.endArray();
-			
+
 			resp.setContentType("application/json");
 			resp.getWriter().println(jsonStringer.toString());
 		} catch (Exception e) {
@@ -117,50 +149,48 @@ public class ListingServlet extends HttpServlet {
 		}
 
 	}
-	
-	public static List<FoodListing> getMyFoodListings()
-	{
+
+	public static List<FoodListing> getMyFoodListings() {
 		List<FoodListing> result = new ArrayList<FoodListing>();
 		FoodMoverUser me = FoodMoverUser.getCurrentUser();
 		Query q = new Query("FoodListing");
 		q.addFilter("owner", Query.FilterOperator.EQUAL, me.getRawUserObject());
-		
+
 		DatastoreService data = DatastoreServiceFactory.getDatastoreService();
 		PreparedQuery prepQ = data.prepare(q);
-		
+
 		for (Entity found : prepQ.asIterable()) {
 			// Longitude check in memory
 			FoodListing resultItem = new FoodListing(found);
 			result.add(resultItem);
 		}
-		
+
 		return result;
 	}
 
-	public static List<FoodListing> findFoodListings(Float longitude, Float latitude, Float distance)
-	{
+	public static List<FoodListing> findFoodListings(Float longitude, Float latitude, Float distance) {
 		List<FoodListing> result = new ArrayList<FoodListing>();
-		
+
 		Query q = new Query("FoodListing");
-		
+
 		// Latitude check with query
 		q.addFilter(FoodListing.LAT_KEY, Query.FilterOperator.GREATER_THAN_OR_EQUAL, latitude - distance);
 		q.addFilter(FoodListing.LAT_KEY, Query.FilterOperator.LESS_THAN_OR_EQUAL, latitude + distance);
-		
+
 		DatastoreService data = DatastoreServiceFactory.getDatastoreService();
 		PreparedQuery prepQ = data.prepare(q);
-		
+
 		for (Entity found : prepQ.asIterable()) {
 			// Longitude check in memory
 			FoodListing resultItem = new FoodListing(found);
-			
-			if(resultItem.getLongitude() >= (longitude -distance) && resultItem.getLongitude() <= (longitude + distance)){
+
+			if (resultItem.getLongitude() >= (longitude - distance)
+					&& resultItem.getLongitude() <= (longitude + distance)) {
 				result.add(resultItem);
 			}
 		}
-		
+
 		return result;
 	}
-	
 
 }
--- war/templates/header.jsp
+++ war/templates/header.jsp
@@ -30,6 +30,7 @@
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+		<script type="text/javascript" src="js/url-param.js"></script>
 		<link rel="stylesheet" href="css/style.css"/>
 		<link rel="stylesheet" type="text/css" href="css/style.css"/>
         <link rel="stylesheet" href="css/pageStyle.css"/>
